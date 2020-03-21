ac803d2d00899497448aecd32fc2e386331ff801
John Leimgruber <leimgrub@gmail.com>
2020-03-21 19:59:50 -0400

    basic input and output

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ I can't believe its not Matlab!
 docker run --rm -it -v "$PWD":/data mtmiller/octave octave /data/myscript.m
 ```
 
-
-
 ## References
 * [mtmiller/octave](https://hub.docker.com/r/mtmiller/octave)
+* [stack exchange problem](https://dsp.stackexchange.com/questions/41556/matlab-phase-of-2d-rectangular-pulses-fourier-transform)
+* [mathworks help](https://www.mathworks.com/help/matlab/math/two-dimensional-fft.html)
--- myscript.m
+++ myscript.m
@@ -4,18 +4,25 @@
 masksize = 512;
 
 # desired scale factor
-scale = 2;
+scale = 10;
 
 # create a meshgrid over which to evaluate the functions
 x = linspace(-scale, scale, masksize);
 y = x;
 [X,Y] = meshgrid(x);
 
-# evaluate a given function
+# evaluate a given input function
 F = X .* exp(-X .^ 2 - Y .^ 2);
 
-# plot the given function
-aux=figure();
+# make a 3d surface plot the given input function
+infig=figure();
 surf(X,Y,F);
 shading interp;
-saveas(aux, '/data/output.png', 'png');
+saveas(infig, '/data/input.png', 'png');
+
+# make a 2d image plot of magnitude of 2D FFT of above input function
+outfig=figure();
+DP = fftshift(fft2(F));
+imagesc(abs(DP));
+axis image;
+saveas(outfig, '/data/output.png', 'png');
